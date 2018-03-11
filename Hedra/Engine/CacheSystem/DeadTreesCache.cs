de6d9eddaa3a2c17afe910c7dc94c02c2231eeae
Zaphyk <zaphyk@live.com>
2018-03-11 03:59:13 -0300

    added new dead tree models

--- Hedra/Engine/CacheSystem/DeadTreesCache.cs
+++ Hedra/Engine/CacheSystem/DeadTreesCache.cs
@@ -8,7 +8,13 @@ namespace Hedra.Engine.CacheSystem
         public DeadTreesCache()
         {
             this.AddModel(AssetManager.PlyLoader("Assets/Env/DeadTree0.ply", Vector3.One));
+            this.AddModel(AssetManager.PlyLoader("Assets/Env/DeadTree1.ply", Vector3.One));
+            this.AddModel(AssetManager.PlyLoader("Assets/Env/DeadTree2.ply", Vector3.One));
+            this.AddModel(AssetManager.PlyLoader("Assets/Env/DeadTree3.ply", Vector3.One));
 
+            this.AddShapes(AssetManager.LoadCollisionShapes("Assets/Env/DeadTree0.ply", 2, Vector3.One));
+            this.AddShapes(AssetManager.LoadCollisionShapes("Assets/Env/DeadTree0.ply", 2, Vector3.One));
+            this.AddShapes(AssetManager.LoadCollisionShapes("Assets/Env/DeadTree0.ply", 2, Vector3.One));
             this.AddShapes(AssetManager.LoadCollisionShapes("Assets/Env/DeadTree0.ply", 2, Vector3.One));
         }
     }
