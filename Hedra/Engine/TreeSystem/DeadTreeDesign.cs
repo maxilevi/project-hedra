b78df8120f51eb7c1c7cc50ff4fc9dedc60efe47
Zaphyk <zaphyk@live.com>
2018-03-11 04:00:05 -0300

    added a new tree design

---
+++ Hedra/Engine/TreeSystem/DeadTreeDesign.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Hedra.Engine.CacheSystem;
+using Hedra.Engine.Management;
+using Hedra.Engine.Rendering;
+using OpenTK;
+
+namespace Hedra.Engine.TreeSystem
+{
+    public class DeadTreeDesign : TreeDesign
+    {
+        public override float Spacing => 60f;
+        public override VertexData Model => CacheManager.GetModel(CacheItem.DeadTrees);
+        public override VertexData Paint(VertexData Data, Vector4 WoodColor, Vector4 LeafColor)
+        {
+            Data.Color(AssetManager.ColorCode0, WoodColor);
+            Data.ExtraData = new List<float>();
+            Data.ExtraData.AddRange(Data.GenerateWindValues());
+
+            return Data;
+        }
+    }
+}
