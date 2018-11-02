350c0bf0b375f31fa29c83bb1a4de7a531075bee
maximiliano.levi <zaphyk@live.com>
2018-11-02 14:19:08 -0300

    added ItemPool.cs tests

--- HedraTests/ItemSystem/ItemPoolTest.cs
+++ HedraTests/ItemSystem/ItemPoolTest.cs
@@ -1,5 +1,7 @@
+using Hedra;
 using Hedra.Engine.Game;
 using Hedra.Engine.ItemSystem;
+using NUnit.Framework;
 
 namespace HedraTests.ItemSystem
 {
@@ -10,19 +12,29 @@ namespace HedraTests.ItemSystem
             ItemFactory.LoadModules(GameLoader.AppPath);
         }
 
+        [Test]
         public void TestItemPoolGivesLessOrEqualRarity()
         {
-            
+            for (var i = 0; i < (int) ItemTier.Misc; i++)
+            {
+                var tier = ItemPool.Grab(new ItemPoolSettings((ItemTier) i)).Tier;
+                Assert.LessOrEqual((int)tier, i, $"Expected tier '{(ItemTier) i}' but got tier {tier}");
+            }
         }
         
-        public void TestItemPoolRandomizing()
-        {
-            
-        }
-        
+        [Test]
         public void TestItemPoolSeed()
         {
-            
+            var seed = Utils.Rng.Next(0, 9999999);
+            var item = ItemPool.Grab(new ItemPoolSettings(ItemTier.Divine)
+            {
+                Seed = seed
+            });
+            var newItem = ItemPool.Grab(new ItemPoolSettings(ItemTier.Divine)
+            {
+                Seed = seed
+            });
+            ItemTest.AssertAreSame(item, newItem);
         }
     }
 }
