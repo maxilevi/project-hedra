e69ce17a4aa2daf8e2ab447f1284c30f6ac40d09
maxilevi <maxilevi77@gmail.com>
2019-10-03 13:30:25 -0300

    fixed deadlock on background updater

--- Hedra/Engine/Player/BackgroundUpdater.cs
+++ Hedra/Engine/Player/BackgroundUpdater.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -16,10 +17,11 @@ namespace Hedra.Engine.Player
     {
         private static Thread _updateThread;
         private static Stopwatch _watch;
-        private static object _updateLock;
         private static List<IUpdatable> _updateList;
         private static TickSystem _tickSystem;
         private static AutoResetEvent _resetEvent;
+        private static ConcurrentQueue<ITickable> _toAddTickables, _toRemoveTickables;
+        private static ConcurrentQueue<IUpdatable> _toAddUpdatables, _toRemoveUpdatables;
         
         public static void Load()
         {
@@ -27,8 +29,11 @@ namespace Hedra.Engine.Player
             _updateThread = new Thread(Update);
             _watch = new Stopwatch();
             _updateList = new List<IUpdatable>();
-            _updateLock = new object();
             _resetEvent = new AutoResetEvent(false);
+            _toAddTickables = new ConcurrentQueue<ITickable>();
+            _toRemoveTickables = new ConcurrentQueue<ITickable>();
+            _toAddUpdatables = new ConcurrentQueue<IUpdatable>();
+            _toRemoveUpdatables = new ConcurrentQueue<IUpdatable>();
         }
         
         public static void Dispatch()
@@ -91,46 +96,71 @@ namespace Hedra.Engine.Player
 
         private static void UpdateCommands()
         {
-            lock (_updateLock)
+            AddTickables();
+            AddUpdatables();
+            RemoveTickables();
+            RemoveUpdatables();
+            for (var i = 0; i < _updateList.Count; ++i)
             {
-                for (var i = 0; i < _updateList.Count; ++i)
-                {
-                    _updateList[i].Update();
-                }
+                _updateList[i].Update();
             }
             _tickSystem.Tick();
         }
 
+        private static void AddTickables()
+        {
+            while (!_toAddTickables.IsEmpty)
+            {
+                var result = _toAddTickables.TryDequeue(out var tickable);
+                if(result) _tickSystem.Add(tickable);
+            }
+        }
+
+        private static void AddUpdatables()
+        {
+            while (!_toAddUpdatables.IsEmpty)
+            {
+                var result = _toAddUpdatables.TryDequeue(out var updatable);
+                if(result) _updateList.Add(updatable);
+            }
+        }
+
+        private static void RemoveTickables()
+        {
+            while (!_toRemoveTickables.IsEmpty)
+            {
+                var result = _toRemoveTickables.TryDequeue(out var tickable);
+                if(result) _tickSystem.Remove(tickable);
+            }
+        }
+
+        private static void RemoveUpdatables()
+        {
+            while (!_toRemoveUpdatables.IsEmpty)
+            {
+                var result = _toRemoveUpdatables.TryDequeue(out var updatable);
+                if(result) _updateList.Remove(updatable);
+            }
+        }
+
         public static void Add(IUpdatable Update)
         {
-            lock (_updateLock)
-            {
-                _updateList.Add(Update);
-            }
+            _toAddUpdatables.Enqueue(Update);
         }
 
         public static void Remove(IUpdatable Update)
         {
-            lock (_updateLock)
-            {
-                _updateList.Remove(Update);
-            }
+            _toRemoveUpdatables.Enqueue(Update);
         }
         
         public static void Add(ITickable Tickable)
         {
-            lock (_updateLock)
-            {
-                _tickSystem.Add(Tickable);
-            }
+            _toAddTickables.Enqueue(Tickable);
         }
 
         public static void Remove(ITickable Tickable)
         {
-            lock (_updateLock)
-            {
-                _tickSystem.Remove(Tickable);
-            }
+            _toRemoveTickables.Enqueue(Tickable);
         }
     }
 }
