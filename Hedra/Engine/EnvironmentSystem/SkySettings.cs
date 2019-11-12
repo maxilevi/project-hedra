9fe40fe318c1a5709e68c0ab92c0cd5f59702642
maxilevi <maxilevi77@gmail.com>
2019-11-12 16:17:53 -0300

    fixed time handling and removed interpolation

---
+++ Hedra/Engine/EnvironmentSystem/SkySettings.cs
@@ -0,0 +1,9 @@
+namespace Hedra.Engine.EnvironmentSystem
+{
+    public class SkySettings
+    {
+        public float DayTime { get; set; } = 12000;
+        public bool Enabled { get; set; } = true;
+        public bool UpdateDayColors { get; set; } = true;
+    }
+}
--- Hedra/Engine/StructureSystem/Overworld/TimeHandler.cs
+++ Hedra/Engine/StructureSystem/Overworld/TimeHandler.cs
@@ -10,15 +10,13 @@ namespace Hedra.Engine.StructureSystem.Overworld
 {
     public class TimeHandler
     {
-        private int _targetSkyTime;
-        private SoundType _soundType;
+        private readonly int _targetSkyTime;
+        private readonly SoundType _soundType;
         private bool _restoreSoundPlayed;
         private bool _isInsideZone;
-        private float _previousTime;
-        private float _targetTime;
-        private float _beforeSaveTime;
-        private float _oldTime;
-        private bool _shouldUpdateTime;
+        private int _beforeSaveTime;
+        private int _previousTime;
+        private SkySettings _settings;
 
         public bool IsActive => _isInsideZone;
         
@@ -38,11 +36,9 @@ namespace Hedra.Engine.StructureSystem.Overworld
         public void Apply()
         {
             if(_isInsideZone) return;
-            _previousTime = SkyManager.DayTime;
-            if(_previousTime < 12000) SkyManager.DayTime += 24000;
+            SkyManager.PushTime();
             SkyManager.Enabled = false;
-            _targetTime = _targetSkyTime;
-            _shouldUpdateTime = true;
+            SkyManager.DayTime = _targetSkyTime;
             SoundPlayer.PlayUISound(_soundType);
             _isInsideZone = true;
         }
@@ -50,55 +46,34 @@ namespace Hedra.Engine.StructureSystem.Overworld
         public void Remove()
         {
             if(!_isInsideZone) return;
-            _targetTime = _previousTime;
-            if(_previousTime < 12000) _targetTime += 24000;
-            _shouldUpdateTime = true;
-            SkyManager.Enabled = false;
+            SkyManager.PopTime();
             _isInsideZone = false;
         }
              
         private void HandleSky()
         {
-            if(_shouldUpdateTime)
+            /*
+            if (Math.Abs(float.MaxValue - _targetTime) > 0.005f)
             {
-                
-                SkyManager.SetTime( Mathf.Lerp(SkyManager.DayTime, _targetTime, Time.DeltaTime * 2f) );
-                if( Math.Abs(SkyManager.DayTime - _targetTime) < 10 )
+                SkyManager.SetTime(Mathf.Lerp(SkyManager.DayTime, _targetTime, (float)Time.DeltaTime * 2f));
+                if (Math.Abs(SkyManager.DayTime - _targetTime) < 10)
                 {
-                    _shouldUpdateTime = false;
-                    SkyManager.Enabled = true;
-                    if( SkyManager.DayTime > 24000) SkyManager.DayTime -= 24000;
+                    if (SkyManager.DayTime > 24000) SkyManager.DayTime -= 24000;
                 }
-            }
+            }*/
         }
         
         private void BeforeSave(object Invoker, EventArgs Args)
         {
-            if(_isInsideZone)
-            {
-                _beforeSaveTime = SkyManager.DayTime;
-                SkyManager.DayTime = _previousTime;
-            }
-            _oldTime = float.MaxValue;
-            if (SkyManager.StackLength > 0)
-            {
-                _oldTime = SkyManager.PeekTime();
-                SkyManager.PopTime();
-            }
+            if (_isInsideZone)
+                _settings = SkyManager.PopTime();
         }
         
         private void AfterSave(object Invoker, EventArgs Args)
         {
             if(_isInsideZone)
-            {
-                SkyManager.DayTime = _beforeSaveTime;
-            }
-
-            if (_oldTime != float.MaxValue)
-            {
-                SkyManager.DayTime = _oldTime;
-                SkyManager.PushTime();
-            }
+                SkyManager.PushTime(_settings);
+            _settings = null;
         }
 
         public void Dispose()
