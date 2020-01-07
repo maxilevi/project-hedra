18d2c021826b1f63da7ee8a0bf51ce7b551c5e9b
maxilevi <maxilevi77@gmail.com>
2020-01-07 12:49:36 -0300

    reduce spacing

--- Hedra/Engine/TreeSystem/BirchDesign.cs
+++ Hedra/Engine/TreeSystem/BirchDesign.cs
@@ -7,7 +7,7 @@ namespace Hedra.Engine.TreeSystem
 {
     public class BirchDesign : TreeDesign
     {
-        public override float Spacing => 70;
+        public override float Spacing => 50;
         public override VertexData Model => CacheManager.GetModel(CacheItem.BirchTrees);
         public override VertexData Paint(VertexData Data, Vector4 WoodColor, Vector4 LeafColor)
         {
