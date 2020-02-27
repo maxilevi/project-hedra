66c19d95a047a9699242321dc45a53de097f73f3
maxilevi <maxilevi77@gmail.com>
2020-02-27 01:43:45 -0300

    added garrison meshes into thegame

--- Hedra/Engine/CacheSystem/GarrisonCache.cs
+++ Hedra/Engine/CacheSystem/GarrisonCache.cs
@@ -9,8 +9,16 @@ namespace Hedra.Engine.CacheSystem
 
         public GarrisonCache()
         {
-            AddModel(AssetManager.PLYLoader("Assets/Env/Structures/Garrison/Garrison0.ply", Vector3.One));
+            AddModel(AssetManager.PLYLoader("Assets/Env/Structures/Garrison/Garrison0-Mesh.ply", Vector3.One));
             AddShapes(AssetManager.LoadCollisionShapes("Assets/Env/Structures/Garrison/Garrison0.ply", Vector3.One));
         }
+
+        public static Vector3[] Doors { get; } = new Vector3[4]
+        {
+            new Vector3(-69.41303f, 7.28182f, 20.79085f),
+            new Vector3(-23.36839f, 8.57924f, -11.57599f),
+            new Vector3(67.29353f, 7.25386f, -49.92051f),
+            new Vector3(26.6138f, 38.44756f, -15.21347f)
+        };
     }
 }
--- Hedra/Engine/StructureSystem/Overworld/GarrisonDesign.cs
+++ Hedra/Engine/StructureSystem/Overworld/GarrisonDesign.cs
@@ -3,6 +3,7 @@ using System.Numerics;
 using Hedra.BiomeSystem;
 using Hedra.Engine.CacheSystem;
 using Hedra.Engine.Generation;
+using Hedra.Engine.Management;
 using Hedra.Numerics;
 using Hedra.Rendering;
 
@@ -15,6 +16,22 @@ namespace Hedra.Engine.StructureSystem.Overworld
         public override bool CanSpawnInside => false;
         protected override int StructureChance => StructureGrid.GarrisonChance;
         protected override CacheItem? Cache => CacheItem.Garrison;
+
+        protected override void DoBuild(CollidableStructure Structure, Matrix4x4 Rotation, Matrix4x4 Translation,
+            Random Rng)
+        {
+            base.DoBuild(Structure, Rotation, Translation, Rng);
+            /* Office */
+            AddDoor(AssetManager.PLYLoader($"Assets/Env/Structures/Garrison/Garrison0-Door0.ply", Vector3.One),
+                GarrisonCache.Doors[0], Rotation, Structure, true, true);
+            AddDoor(AssetManager.PLYLoader($"Assets/Env/Structures/Garrison/Garrison0-Door1.ply", Vector3.One),
+                GarrisonCache.Doors[1], Rotation, Structure, true, true);
+            AddDoor(AssetManager.PLYLoader($"Assets/Env/Structures/Garrison/Garrison0-Door2.ply", Vector3.One),
+                GarrisonCache.Doors[2], Rotation, Structure, true, true);
+            AddDoor(AssetManager.PLYLoader($"Assets/Env/Structures/Garrison/Garrison0-Door3.ply", Vector3.One),
+                GarrisonCache.Doors[3], Rotation, Structure, true, true);
+        }
+
         protected override Garrison Create(Vector3 Position, float Size)
         {
             return new Garrison(Position);
