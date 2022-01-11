d6dab018361441bba924d999628cb39c55740d45
maxilevi <maxilevi77@gmail.com>
2022-01-10 22:44:40 -0300

    add publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+RID=$1
+FRAMEWORK=netcoreapp6.0
+
+valid_rids=("win-x64" "win-x86" "linux-x64")
+
+if ! [[ $(echo ${valid_rids[@]} | grep -o $RID | wc -w) ]]; then
+	exit 1
+fi
+
+(cd ../Hedra && dotnet publish -c Release -r $RID --self-contained true)
+
+rm -rf ../Hedra/bin/Release/$FRAMEWORK/$RID/Assets
+rm -rf ../Hedra/bin/Release/$FRAMEWORK/$RID/Shaders
+rm -rf ../Hedra/bin/Release/$FRAMEWORK/$RID/Sounds
+rm -rf ../Hedra/bin/Release/$FRAMEWORK/$RID/ref
+
