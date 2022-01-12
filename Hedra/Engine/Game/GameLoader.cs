ccb87f25beab189202fb6d4befa69012ad997119
maxilevi <maxilevi77@gmail.com>
2022-01-11 23:03:29 -0300

    test manu8ally resolving steam dep

--- Hedra/Engine/Game/GameLoader.cs
+++ Hedra/Engine/Game/GameLoader.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using Facepunch.Steamworks;
 using Hedra.Engine.IO;
 using Hedra.Engine.Management;
 using Hedra.Engine.Native;
@@ -112,8 +113,9 @@ namespace Hedra.Engine.Game
                 TryLoad($"{dllPath}/{prefix}steam_api{(is64 ? "64" : string.Empty)}.{ext}"),
                 TryLoad($"{dllPath}/libbulletc.{ext}"),
                 TryLoad($"{dllPath}/openal32.{ext}"),
-                TryLoad($"{dllPath}/{prefix}glfw3.{ext}"),
+                //TryLoad($"{dllPath}/{prefix}glfw3.{ext}"),
             };
+            NativeLibrary.SetDllImportResolver(Assembly.GetAssembly(typeof(BaseSteamworks)), DllImportResolver);
             _loadedArchitectureFiles = true;
         }
 
@@ -124,6 +126,15 @@ namespace Hedra.Engine.Game
             else 
                 Log.WriteLine($"Successfully loaded {Lib}");
             return outLib;
+        }
+        
+        private static IntPtr DllImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
+        {
+            Log.WriteLine(libraryName);
+            if (libraryName != "libsteam_api") return IntPtr.Zero;
+            var is64 = Environment.Is64BitProcess;
+            var dllPath = $"{AppPath}{(is64 ? "x64" : "x86")}";
+            return NativeLibrary.Load($"{dllPath}/libsteam_api64.so");
         }
 
         public static void UnloadNativeLibs()
