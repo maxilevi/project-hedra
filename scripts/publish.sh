169cf96374ae93e889da564b9f214913267be709
maxilevi <maxilevi77@gmail.com>
2022-01-12 00:02:55 -0300

    fix publishing

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-cd "$(dirname "$0")"
-
 RID=$1
 FRAMEWORK=netcoreapp6.0
 
-dotnet publish ../Hedra/Hedra.csproj -c Release -r $RID --self-contained true
+dotnet publish Hedra/Hedra.csproj -c Release -r $RID --self-contained true
 
-rm -rf ../Hedra/bin/Release/$FRAMEWORK/$RID/publish/Assets
-rm -rf ../Hedra/bin/Release/$FRAMEWORK/$RID/publish/Shaders
-rm -rf ../Hedra/bin/Release/$FRAMEWORK/$RID/publish/Sounds
-rm -rf ../Hedra/bin/Release/$FRAMEWORK/$RID/publish/ref
+rm -rf Hedra/bin/Release/$FRAMEWORK/$RID/publish/Assets
+rm -rf Hedra/bin/Release/$FRAMEWORK/$RID/publish/Shaders
+rm -rf Hedra/bin/Release/$FRAMEWORK/$RID/publish/Sounds
+rm -rf Hedra/bin/Release/$FRAMEWORK/$RID/publish/ref
 
