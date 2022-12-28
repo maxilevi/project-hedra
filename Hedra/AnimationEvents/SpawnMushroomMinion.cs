c8915a13b0aacee687428db783496498e1a171d8
Maximiliano Levi <maxilevi77@gmail.com>
2022-12-28 00:34:39 -0300

    remove biped hound

--- Hedra/AnimationEvents/SpawnMushroomMinion.cs
+++ Hedra/AnimationEvents/SpawnMushroomMinion.cs
@@ -10,7 +10,7 @@ public class SpawnMushroomMinion : SpawnMinion
     protected override IEntity CreateMinion(Vector3 Position)
     {
         var mobSeed = Utils.Rng.Next(int.MaxValue);
-        var type = Utils.Rng.NextBool() ? MobType.MushroomHound : MobType.MushroomHoundBiped;
+        var type = MobType.MushroomHound;//Utils.Rng.NextBool() ? MobType.MushroomHound : MobType.MushroomHoundBiped;
         return World.SpawnMob(type.ToString(), Position, mobSeed);
     }
 
