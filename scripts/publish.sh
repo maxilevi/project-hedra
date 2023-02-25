33b7a6ef17fe76529bf48bbc7d47a2fab1ded58c
Maximiliano Levi <8160966+maxilevi@users.noreply.github.com>
2023-02-25 16:07:33 -0300

    Update publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,8 +4,9 @@ dotnet --info
 
 RID=$1
 FRAMEWORK=netcoreapp6.0
+CONFIG=Debug
 
-dotnet publish Hedra/Hedra.csproj -c Release -r $RID --self-contained true
+dotnet publish Hedra/Hedra.csproj -c $CONFIG -r $RID --self-contained true
 
 rm -rf Hedra/bin/Release/$FRAMEWORK/$RID/publish/Assets
 rm -rf Hedra/bin/Release/$FRAMEWORK/$RID/publish/Shaders
